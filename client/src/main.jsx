c35aa438308c3050f4a52f68b4bc34969add8fb2
Anubhab <anubhab.techie@gmail.com>
2023-03-09 16:27:53 +0530

    Added client

---
+++ client/src/main.jsx
@@ -0,0 +1,16 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import App from "./App";
+import "./index.css"
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <ThirdwebProvider chainId={ChainId.Goerli}>
+    <Router>
+      <App />
+    </Router>
+  </ThirdwebProvider>
+);
